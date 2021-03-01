b8039788aedf67c19555d6643a49e3cf89d12ea4
Nicholas Surmava <nicholas.surmava@gmail.com>
2021-03-01 17:33:50 -0600

    added menu for da pretty patties

---
+++ config.js
@@ -0,0 +1,28 @@
+const menu = [
+  "Green",
+  "Magenta",
+  "Red",
+  "Blue",
+  "Orange",
+  "Purple",
+  "Yellow",
+  "Tan",
+  "Beige",
+  "Turquoise",
+  "Dark green",
+  "Dark brown",
+  "A dull blue-gray",
+  "Plaid",
+  "Orange with green spots",
+  "Pink",
+  "Green glow in the dark",
+  "Rainbow (all the colors in up and down stripes)",
+  "Tartan",
+  "Green with black and purple stripes",
+  "Silver",
+  "Gold",
+  "Animal print",
+  "Invisible",
+];
+
+module.exports = menu;
