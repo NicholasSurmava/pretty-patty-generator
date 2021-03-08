fbeaa15e5aeda577f77c845cdd6724f7360ea082
Nicholas Surmava <nicholas.surmava@gmail.com>
2021-03-08 06:35:05 -0600

    added some images of patties, configured the static dir

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ const menu = require("../config");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.static(path.join(__dirname, "public")));
+
 let takeOrder = async () => {
   return new Promise((done) => {
-    console.log("May I take your order?");
     console.log("Order taken!");
     done();
   });
@@ -16,10 +17,8 @@ let takeOrder = async () => {
 
 let createPrettyPatty = async (menu) => {
   return new Promise((done) => {
-    setTimeout(() => {
-      console.log(`1 ${menu} pretty patty ready!`);
-      done();
-    }, 2000);
+    console.log(`1 ${menu} pretty patty ready!`);
+    done();
   });
 };
 
